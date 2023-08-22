a6e183309d756a8c83f7fc260a2a00e87b3d528f
김동영 <doyokim75@gmail.com>
2023-08-22 20:03:39 +0900

    230822 2003

--- SWEA/0822/실습/1232 사칙연산.py
+++ SWEA/0822/실습/1232 사칙연산.py
@@ -1,9 +1,39 @@
 import sys
 sys.stdin = open('cal.txt')
 
+def postorder(n):
+    # if not tree[n][0] and not tree[n][1]:
+    #     return tree[n][3]
+    if n:
+        a = postorder(tree[n][0])
+        b = postorder(tree[n][1])
+        if tree[n][3] == '+':
+            return a + b
+        elif tree[n][3] == '-':
+            return a - b
+        elif tree[n][3] == '*':
+            return a * b
+        elif tree[n][3] == '/':
+            return a // b
+        else:
+            return tree[n][3]
+    # return 0
+
+
 T = 10
 for tc in range(1, T+1):
     N = int(input())
-    # tree = [[0] * 3 for _ in range(N)]
+
+    tree = [[0] * 4 for _ in range(N+1)]
     for i in range(N):
-        pass
+        temp = input().split()
+        if len(temp) == 4:
+            tree[int(temp[0])][3] = temp[1]
+            tree[int(temp[0])][0] = int(temp[2])
+            tree[int(temp[0])][1] = int(temp[3])
+            tree[int(temp[2])][2] = int(temp[0])
+            tree[int(temp[3])][2] = int(temp[0])
+        else:
+            tree[int(temp[0])][3] = int(temp[1])
+
+    print(f'#{tc} {postorder(1)}')
