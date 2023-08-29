9d9287f56e7dfd539d7b7efb6cd49425c696d0f4
K.Dongyoung <doyokim75@gmail.com>
2023-08-29 17:56:58 +0900

    230829 1756

---
+++ SWEA/0829/2817 부분 수열의 합.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('sum.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    count = 0
+    for i in range(1<<N):
+        sum = 0
+        for j in range(N):
+            if i & (1<<j):
+                sum += A[j]
+        if sum == K:
+            count += 1
+
+    print(f'#{tc} {count}')
