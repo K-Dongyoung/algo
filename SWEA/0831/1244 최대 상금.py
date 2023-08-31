36441538f4f0b14f222a101172966deba396d49c
K.Dongyoung <doyokim75@gmail.com>
2023-08-31 18:00:42 +0900

    230831 1800

--- SWEA/0831/1244 최대 상금.py
+++ SWEA/0831/1244 최대 상금.py
@@ -2,25 +2,24 @@ import sys
 sys.stdin = open('prize.txt')
 
 def f():
-    maxi = 0
     cnt = 0
-    for i in range(len(number)):
-        for j in range(i, len(number)):
+    for i in range(len(number)-1):
+        maxi = i
+        for j in range(i+1, len(number)):
             if number[maxi] <= number[j]:
                 maxi = j
         number[i], number[maxi] = number[maxi], number[i]
         cnt += 1
         if cnt == change:
-            break
-    print(number)
-    if cnt < change:
-        for i in range(len(number)-1):
-            for j in range(i+1, len(number)):
-                if number[i] == number[j]:
-                    return
-        for i in range(change-cnt):
-            number[-1], number[-2] = number[-2], number[-1]
-    print(number)
+            return
+
+    for i in range(len(number)-1):
+        for j in range(i+1, len(number)):
+            if number[i] == number[j]:
+                return
+    for i in range(change-cnt):
+        number[-1], number[-2] = number[-2], number[-1]
+
 
 T = int(input())
 for tc in range(1, T+1):
@@ -28,4 +27,4 @@ for tc in range(1, T+1):
     change = int(change)
     number = list(map(int, number))
     f()
-
+    print(number)
