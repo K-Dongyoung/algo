04f4d160323639eaa44d9355c2ef1c91e968b9fd
Kim, Dongyoung <doyokim75@gmail.com>
2023-09-18 17:45:29 +0900

    230918 1745

--- SWEA/0918/11772 이진 탐색.py
+++ SWEA/0918/11772 이진 탐색.py
@@ -9,11 +9,29 @@ for tc in range(1, T+1):
     A.sort()
     B = list(map(int, input().split()))
     cnt = 0
-    r = 0
-    l = 0
+    stack = []
     for b in B:
         s = 0
         e = len(A)-1
         while s <= e:
             m = (s + e) // 2
-            if m == b :
+
+            if A[m] == b:
+                cnt += 1
+                break
+
+            elif A[m] > b:
+                e = m - 1
+                if stack and stack[-1] == 'l':
+                    break
+                else:
+                    stack.append('l')
+
+            else:
+                s = m + 1
+                if stack and stack[-1] == 'r':
+                    break
+                else:
+                    stack.append('r')
+
+    print(cnt)
