1d40fd42715b98a4d7ca59a85411e9899219baeb
Kim, Dongyoung <doyokim75@gmail.com>
2023-10-24 16:41:28 +0900

    231024 1641

--- 백준/2차원 배열/2563 색종이.py
+++ 백준/2차원 배열/2563 색종이.py
@@ -1,4 +1,14 @@
 N = int(input())
 
+graph = [[0] * 100 for _ in range(100)]
+sum = 0
+
 for _ in range(N):
     x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                sum += 1
+
+print(sum)
