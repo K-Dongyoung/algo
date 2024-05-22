207329b37aeb5dff5a64f754f6b73f9df3860af8
Dongyoung <doyokim75@gmail.com>
2024-05-22 09:51:37 +0900

    벌집 - 2 (백준 - 2292)

--- 백준/일반 수학1/2292 벌집.py
+++ 백준/일반 수학1/2292 벌집.py
@@ -1,15 +1,9 @@
 N = int(input())
 level = 1
-answer = 0
 ceiling = 1
 
-while True:
-
-    if N <= ceiling:
-        answer = level
-        break
-
+while N > ceiling:
     ceiling += 6 * level
     level += 1
 
-print(answer)
+print(level)
