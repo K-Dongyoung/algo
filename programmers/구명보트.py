2cf1fcc270d0d8e731763aa7e77914af391a4537
K.Dongyoung <doyokim75@gmail.com>
2024-08-07 09:41:11 +0900

    240807 구명보트 (programmers)

--- programmers/구명보트.py
+++ programmers/구명보트.py
@@ -5,12 +5,9 @@ def solution(people, limit):
     e = len(people) - 1
     while s <= e:
         if people[s] + people[e] <= limit:
-            ans += 1
             s += 1
-            e -= 1
-        else:
-            ans += 1
-            e -= 1
+        ans += 1
+        e -= 1
     return ans
 
 
