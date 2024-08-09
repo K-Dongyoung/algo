61aa6dd74f989b37941f11d571bfaf0f41c60310
K.Dongyoung <doyokim75@gmail.com>
2024-08-09 09:34:43 +0900

    240809 연속 부분 수열 합의 개수 - refactor (programmers)

--- programmers/연속 부분 수열 합의 개수.py
+++ programmers/연속 부분 수열 합의 개수.py
@@ -8,4 +8,27 @@ def solution(elements):
     return len(s) + 1
 
 
-print(solution([7, 9, 1, 1, 4]))
+def solution2(elements):
+    length_elements = len(elements)
+    a = elements + elements[:length_elements - 1]
+    s = set()
+    for i in range(length_elements):
+        b = 0
+        for j in range(length_elements - 1):
+            b += a[i + j]
+            s.add(b)
+    return len(s) + 1
+
+
+def solution3(elements):
+    length_elements = len(elements)
+    s = set()
+    for i in range(length_elements):
+        b = 0
+        for j in range(length_elements - 1):
+            b += elements[(i + j) % length_elements]
+            s.add(b)
+    return len(s) + 1
+
+
+print(solution3([7, 9, 1, 1, 4]))
