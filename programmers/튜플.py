6d59367b72452288b8975de47484022c2bf4830f
K.Dongyoung <doyokim75@gmail.com>
2024-09-12 19:17:47 +0900

    240912 튜플 (programmers)

---
+++ programmers/튜플.py
@@ -0,0 +1,30 @@
+def solution(s):
+    s = s[1:-1]
+    answer = []
+    a = list()
+    b = ""
+
+    for c in s:
+        if c != "{" and c != "}":
+            b += c
+        elif c == "}":
+            a.append(set(map(int, b.split(','))))
+        elif c == "{":
+            b = ""
+
+    a.sort(key=lambda x: len(x))
+
+    prev_s = set()
+    for si in a:
+        n = next(iter(si - prev_s))
+        prev_s = si
+        answer.append(n)
+
+    return answer
+
+
+print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
+print(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))
+print(solution("{{20,111},{111}}"))
+print(solution("{{123}}"))
+print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
